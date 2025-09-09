b811003b3e82d362bdf1c5c6ae99a158ffaaf888
Andy Tran <TylerT124@yahoo.com>
2025-09-08 20:30:45 -0700

    Added more to home page

--- src/app/home_new/page.tsx
+++ src/app/home_new/page.tsx
@@ -107,7 +107,7 @@ export default function HomeNewPage() {
         className="relative mx-auto"
         style={{
           width: '1440px',
-          height: '2000px', // Give the section some height
+          height: '2250px', // Increased height to add more space below TextReveal
         }}
       >
         <div 
@@ -154,6 +154,160 @@ export default function HomeNewPage() {
             />
           </div>
         </div>
+        
+        {/* Fixed Centered Box at bottom of page */}
+        <div 
+          className="absolute left-1/2 transform -translate-x-1/2"
+          style={{
+            bottom: '50px', // 50px from bottom of viewport
+            zIndex: 10 // Ensure it appears above other content
+          }}
+        >
+          <div
+            style={{
+              borderRadius: '30px',
+              background: '#FAF6FC',
+              boxShadow: '0 6px 6px 0 rgba(0, 0, 0, 0.25)',
+              display: 'flex',
+              padding: '50px',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: '25px',
+              alignSelf: 'stretch'
+            }}
+          >
+            {/* Our Mission Title */}
+            <h2 
+              style={{
+                alignSelf: 'stretch',
+                color: '#3A3A3A',
+                textAlign: 'center',
+                fontFamily: 'Inter',
+                fontSize: '40px',
+                fontStyle: 'normal',
+                fontWeight: 600,
+                lineHeight: '48.75px',
+                margin: 0
+              }}
+            >
+              Our Mission
+            </h2>
+            
+            {/* Mission Description */}
+            <p 
+              style={{
+                alignSelf: 'stretch',
+                color: '#3A3A3A',
+                textAlign: 'center',
+                fontFamily: '"M PLUS 1"',
+                fontSize: '22.5px',
+                fontStyle: 'normal',
+                fontWeight: 400,
+                lineHeight: '32px',
+                margin: 0
+              }}
+            >
+              At Product Space, we're a national community of students with a mission to become the world's next generation of product leaders.
+            </p>
+            
+            {/* Meet the Team Link */}
+            <a 
+              href="/about_new"
+              style={{
+                alignSelf: 'stretch',
+                color: '#3A3A3A',
+                textAlign: 'center',
+                fontFamily: '"M PLUS 1"',
+                fontSize: '18.5px',
+                fontStyle: 'normal',
+                fontWeight: 700,
+                lineHeight: '32px',
+                textDecoration: 'none',
+                cursor: 'pointer',
+                margin: 0
+              }}
+            >
+              Meet the Team &gt;
+            </a>
+          </div>
+        </div>
+      </section>
+      
+      {/* New Section - 50px below the box */}
+      <section 
+        style={{
+          position: 'relative', // Enable absolute positioning for children
+          display: 'flex',
+          padding: '150px 0',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '50px',
+          alignSelf: 'stretch',
+          background: '#FAF6FC',
+          marginTop: '50px' // 50px spacing from previous content
+        }}
+      >
+        {/* Recruitment Timeline Container with layered SVGs */}
+        <div 
+          style={{
+            position: 'relative',
+            maxWidth: '100%'
+          }}
+        >
+          {/* Recruitment Timeline Glow SVG - Background Layer */}
+          <img 
+            src="/images/recruitment_timeline_glow.svg" 
+            alt="Recruitment Timeline Glow"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              maxWidth: '100%',
+              height: 'auto',
+              zIndex: 1
+            }}
+          />
+          
+          {/* Recruitment Timeline SVG - Foreground Layer */}
+          <img 
+            src="/images/recruitment_timline.svg" 
+            alt="Recruitment Timeline"
+            style={{
+              position: 'relative',
+              maxWidth: '100%',
+              height: 'auto',
+              zIndex: 2
+            }}
+          />
+        </div>
+        
+        {/* Triangle Glow SVG - Background Layer */}
+        <img 
+          src="/images/rt_triangle_glow.svg" 
+          alt="Triangle Glow"
+          style={{
+            position: 'absolute',
+            left: '712px', // Same positioning as triangle
+            top: '204px',  // Same positioning as triangle
+            width: '170px',
+            height: '106px',
+            zIndex: 9 // Below the triangle
+          }}
+        />
+        
+        {/* Triangle SVG positioned absolutely */}
+        <img 
+          src="/images/rt_triangle.svg" 
+          alt="Triangle"
+          style={{
+            position: 'absolute',
+            left: '712px', // 715px from left of screen
+            top: '204px',  // 165px from top of screen
+            width: '170px',
+            height: '106px',
+            zIndex: 10 // Ensures triangle appears above all other SVGs
+          }}
+        />
       </section>
     </div>
   );
