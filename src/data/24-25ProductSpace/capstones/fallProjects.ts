8f0bd82be3e245166289acd06384443b078a6dbf
Yash Pradhan <yashpradhan181@gmail.com>
2025-09-29 23:05:18 -0700

    figma links put in

--- src/data/24-25ProductSpace/capstones/fallProjects.ts
+++ src/data/24-25ProductSpace/capstones/fallProjects.ts
@@ -13,7 +13,7 @@ export const fallProjects: Project[] = [
     title: "Curate",
     groupMembers: "Amber Zhang, Arshiya Banerjee, Clara Yin, John Missal",
     shortDescription: "Explore content your friends have viewed and shared right in your feed.",
-    onViewPitchDeck: "",
+    onViewPitchDeck: "https://www.figma.com/slides/IwiZwhQrPpzyk2WT70GVmz/Instagram-Curate-%7C-Product-Pitch?node-id=1-346&t=B0B2AJ8qvjmtUf1B-0",
     onViewDesigns: "https://www.figma.com/design/KX3LDR1jHRztBeLaEdHgeA/Instagram-Curate-Page-Mockup?node-id=0-1&t=3RrkFbEO8f4BPl40-1"
   },
   {
@@ -21,7 +21,7 @@ export const fallProjects: Project[] = [
     title: "Sync",
     groupMembers: "Anita Thata, Caitlin Chan, Jason Zhang, Puja Devarasetty",
     shortDescription: "The Instagram Stories you know, just with more voices.",
-    onViewPitchDeck: "",
+    onViewPitchDeck: "https://pitch.com/v/sync-ui7g6i/4bb04205-7490-47b1-b419-028177fdcb1f",
     onViewDesigns: "https://www.figma.com/design/NpSpSjGK3GUjdE9eaxqpWx/Sync-Mockups?node-id=2161-433&t=skRW3kG8nYiVjYDc-1"
   },
   {
@@ -29,7 +29,7 @@ export const fallProjects: Project[] = [
     title: "Scraps",
     groupMembers: "Abirami Kathiresan, Krisha Bhat, Nicole Yanuaria, Sruthi Sridhar, Wing Lai",
     shortDescription: "Showcase your moods and moments on Instagram - your way.",
-    onViewPitchDeck: "",
+    onViewPitchDeck: "https://pitch.com/v/instagram-scraps-pitch-deck-ynf5cj",
     onViewDesigns: "https://www.figma.com/design/YajKvnFULQ9KHlg1OAdAqI/Hi-Fi-Prototype?node-id=0-1&t=6up85hwdxp67io4Q-1"
   },
   {
@@ -37,7 +37,7 @@ export const fallProjects: Project[] = [
     title: "Preview",
     groupMembers: "Alex Sunga, Divleen Lota, Gayatri Brijesh, Phuong Thy Nguyen",
     shortDescription: "Plan and visualize your posts directly through Instagram.",
-    onViewPitchDeck: "https://www.figma.com/design/zUPYykgs0hckZAwkGsnDxT/product-space--team-winx-clerb?node-id=185-8&t=Tv4tmKhpGTkSjpXZ-1",
+    onViewPitchDeck: "https://www.figma.com/proto/0WyRc8yHnwPN3h8WMLulLy/Team-Preview-Fall--24-Capstone---Product-Space?node-id=0-1&t=M2V2M9NmwZ1RcKdm-1",
     onViewDesigns: "https://www.figma.com/design/zUPYykgs0hckZAwkGsnDxT/product-space--team-winx-clerb?node-id=516-3266&t=1sg40nodPnjRv4n3-1"
   }
 ];
--- src/data/24-25ProductSpace/capstones/springProjects.ts
+++ src/data/24-25ProductSpace/capstones/springProjects.ts
@@ -13,7 +13,7 @@ export const springProjects: Project[] = [
     title: "Hone",
     groupMembers: "Amelia Uytingco, Cindy Xing, Taiki Lowe, Zayd Musa",
     shortDescription: "Simplify the off-campus housing search.",
-    onViewPitchDeck: "",
+    onViewPitchDeck: "https://www.canva.com/design/DAGnWYe7KC0/cnehhmQ1_2v9lW3R9Akvjg/edit",
     onViewDesigns: "https://www.figma.com/design/2Mpd4uDIiq18kMgKXMiWyk/team-1-design?node-id=0-1&p=f&t=S3dN6uswUfP9qgJ5-0"
   },
   {
@@ -21,7 +21,7 @@ export const springProjects: Project[] = [
     title: "MOOVE",
     groupMembers: "Anwesha Mohanty, Haolin Zhang, Isaac Villegas, Kavin Agarwal",
     shortDescription: "Stop searching, start mooving.",
-    onViewPitchDeck: "",
+    onViewPitchDeck: "https://docs.google.com/presentation/d/1d3tXbDL3LT13GQy7kHl2O3lnnNMI6DraBZMPhHlQQYA/edit?slide=id.g14ffcd870df_0_0#slide=id.g14ffcd870df_0_0",
     onViewDesigns: "https://www.figma.com/design/o9UaXrM3GYDD1Q8dATKGKY/Moove-Figma?node-id=0-1&t=ecbiN3K4rPFOrYjg-1"
   },
   {
@@ -29,7 +29,7 @@ export const springProjects: Project[] = [
     title: "deCLASSify",
     groupMembers: "Andy Tran, Elango Ketheesan, Sahana Narayan, Yash Pradhan",
     shortDescription: "Connecting students to each other through classes.",
-    onViewPitchDeck: "https://www.canva.com/design/DAGpA8CdfBg/t05az-25eJ4X-puwiaELDg/edit?utm_content=DAGpA8CdfBg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    onViewPitchDeck: "https://www.canva.com/design/DAGpA8CdfBg/t05az-25eJ4X-puwiaELDg/edit?ui=eyJEIjp7IkEiOnsiQSI6IktBR3BFdWdVOWFvIiwiQyI6eyJBPyI6IkEiLCJBIjoiS0FHcEV1Z1U5YW8ifX19fQ",
     onViewDesigns: "https://www.figma.com/design/WNkqTzfAroRkTSOmeY6MYL/deCLASSify-design?node-id=0-1&t=kBm2IHftoLJnjjjL-1"
   },
   {
@@ -37,7 +37,7 @@ export const springProjects: Project[] = [
     title: "Heard",
     groupMembers: "Adrian Rozo, Andrew Fojas, Ken Hoang, Sambhav Agarwal",
     shortDescription: "Enabling anonymous, actionable feedback from UC Davis students.",
-    onViewPitchDeck: "",
+    onViewPitchDeck: "https://www.figma.com/slides/k6ej29MJsVhEmVEJSDTzVq/Heard-Pitch-Deck?node-id=1-1686&t=cKvvLKSzbHQnRYHi-0",
     onViewDesigns: "https://www.figma.com/design/Y97YAHiU8djC50BTqVMjVx/App-Design?node-id=10-6&t=Oc3gGqfTYIsdxZGm-1"
   }
 ];
