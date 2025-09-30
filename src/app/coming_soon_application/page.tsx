ff82ab57b05e4d83ef009d317b949e64ca34941f
Yash Pradhan <yashpradhan181@gmail.com>
2025-09-29 23:24:37 -0700

    temp application dropping soon page

---
+++ src/app/coming_soon_application/page.tsx
@@ -0,0 +1,16 @@
+export default function ComingSoonApplicationPage() {
+  return (
+    <main className="min-h-screen bg-[#FAF6FC] text-[#3a3a3a] flex flex-col">
+      <section className="mt-[150px] flex-grow">
+        <div className="text-center px-5 md:px-0">
+          <h1 className="text-4xl font-bold text-[#3A3A3A] mb-4">
+            Fellowship Application
+          </h1>
+          <p className="text-lg text-[#3A3A3A]">
+            Sit tight! Applications drop on Oct 1st!
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -57,8 +57,9 @@ export default function JoinNewPage() {
             Join Product Space!
           </h1>
         </div>
+        {/* TODO: Change to href="/application" on Oct 1st midnight*/}
         <Link 
-          href="/application"
+          href="/coming_soon_application"
           className="absolute top-[312.125px] left-1/2 transform -translate-x-1/2 z-10 inline-flex px-5 py-3 items-center gap-[15px] rounded-[20px] bg-[#66417B] hover:bg-[#5a3769] transition-colors cursor-pointer"
         >
           <span className="text-[#FAF6FC] text-center font-['M_PLUS_1'] text-[22.5px] font-normal leading-[22.5px]">
